8f5e041cfd1e4579622493892418f0abf9054a20
xgblack <gmg@xgblack.cn>
2019-12-02 15:54:03 +0800

    xml搭建ssm

---
+++ xmlssm/src/main/java/cn/xgblack/controller/HelloController.java
@@ -0,0 +1,22 @@
+package cn.xgblack.controller;
+
+import cn.xgblack.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author xgBLACK
+ * @date 2019/11/29 20:26
+ * description:
+ */
+@RestController
+public class HelloController {
+    @Autowired
+    HelloService helloService;
+
+    @GetMapping(value = "/hello",produces = "text/html;charset=utf-8")
+    public String hello(){
+        return helloService.sayHello();
+    }
+}
