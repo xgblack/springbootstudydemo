770673f712d9282a71c4c31ec0a3ffeb8e468e0c
xgblack <gmg@xgblack.cn>
2019-12-02 15:57:29 +0800

    profile

---
+++ profile_demo/src/main/java/cn/xgblack/profile_demo/ProfileDemoApplication.java
@@ -0,0 +1,13 @@
+package cn.xgblack.profile_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfileDemoApplication.class, args);
+    }
+
+}
---
+++ profile_demo/src/test/java/cn/xgblack/profile_demo/ProfileDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.xgblack.profile_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
