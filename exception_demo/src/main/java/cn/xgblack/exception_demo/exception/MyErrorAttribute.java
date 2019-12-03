1ef209d77a64e709dbebc6e364b9936108a34d09
xgblack <gmg@xgblack.cn>
2019-12-03 10:33:22 +0800

    自定义异常数据

---
+++ exception_demo/src/main/java/cn/xgblack/exception_demo/exception/MyErrorAttribute.java
@@ -0,0 +1,23 @@
+package cn.xgblack.exception_demo.exception;
+
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Map;
+
+/**
+ * @author xg BLACK
+ * @date 2019/12/3 10:29
+ * description:
+ */
+@Component
+public class MyErrorAttribute extends DefaultErrorAttributes {
+    @Override
+    public Map<String, Object> getErrorAttributes(WebRequest webRequest, boolean includeStackTrace) {
+        Map<String, Object> map = super.getErrorAttributes(webRequest, includeStackTrace);
+        map.put("myerror", "自定义的异常信息");
+
+        return map;
+    }
+}
