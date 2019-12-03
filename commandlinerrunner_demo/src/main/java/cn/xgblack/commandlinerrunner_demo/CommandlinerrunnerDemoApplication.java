7471ac78caf2b9fe9db5592d61784c76b4e0d94a
xgblack <gmg@xgblack.cn>
2019-12-03 21:21:01 +0800

    系统启动任务

---
+++ commandlinerrunner_demo/src/main/java/cn/xgblack/commandlinerrunner_demo/CommandlinerrunnerDemoApplication.java
@@ -0,0 +1,13 @@
+package cn.xgblack.commandlinerrunner_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommandlinerrunnerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommandlinerrunnerDemoApplication.class, args);
+    }
+
+}
---
+++ commandlinerrunner_demo/src/main/java/cn/xgblack/commandlinerrunner_demo/MyCommandLineRunner1.java
@@ -0,0 +1,21 @@
+package cn.xgblack.commandlinerrunner_demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+/**
+ * @author xg BLACK
+ * @date 2019/12/3 21:01
+ * description:
+ */
+@Component
+@Order(99)
+public class MyCommandLineRunner1 implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("MyCommandLineRunner1 >>>" + Arrays.toString(args));
+    }
+}
