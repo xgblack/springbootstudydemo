6db69b74a4bbc19c02fc4af26598443b7798d8d2
xgblack <gmg@xgblack.cn>
2019-12-09 14:59:32 +0800

    session共享

---
+++ sessionshare_demo/src/main/java/cn/xgblack/sessionshare_demo/SessionshareDemoApplication.java
@@ -0,0 +1,13 @@
+package cn.xgblack.sessionshare_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionshareDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SessionshareDemoApplication.class, args);
+    }
+
+}
---
+++ sessionshare_demo/src/test/java/cn/xgblack/sessionshare_demo/SessionshareDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.xgblack.sessionshare_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionshareDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
