4562fad303070ca84d5c7fea8834c5aa338b3f4d
xgblack <gmg@xgblack.cn>
2019-12-09 10:58:06 +0800

    整合Redis

---
+++ redis_demo/src/main/java/cn/xgblack/redis_demo/controller/HelloController.java
@@ -0,0 +1,31 @@
+package cn.xgblack.redis_demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author xg BLACK
+ * @date 2019/12/9 10:41
+ * description:
+ */
+@RestController
+public class HelloController {
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+    @GetMapping("/set")
+    public void set(){
+
+        ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
+        ops.set("name","xgBLACK");
+    }
+
+    @GetMapping("/get")
+    public void get(){
+
+        ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
+        System.out.println(ops.get("name"));
+    }
+}
